dd18b142144334ad11ac3164cc5eb3c14a98755b
Ken <137476130+krd022@users.noreply.github.com>
2023-09-24 21:04:50 -0500

    Update DataTypeOperations.java

--- DataTypeOperations.java
+++ DataTypeOperations.java
@@ -4,40 +4,42 @@ public class DataTypeOperations {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // user input data types
+        // user input and print
         System.out.print("Enter an integer: ");
         int userInputInt = scanner.nextInt();
         System.out.println("Integer: " + userInputInt);
 
+        // double input and print
         System.out.print("Enter a double: ");
         double userInputDouble = scanner.nextDouble();
         System.out.println("Double: " + userInputDouble);
 
+        // char input and print
         System.out.print("Enter a character: ");
         char userInputChar = scanner.next().charAt(0);
         System.out.println("Character: " + userInputChar);
         
-
+        // boolean input and print
         System.out.print("Enter a boolean (true or false): ");
         boolean userInputBoolean = scanner.nextBoolean();
         System.out.println("Boolean: " + userInputBoolean);
 
-
+        // float input and print
         System.out.print("Enter a float value: ");
         float floatValue = scanner.nextFloat();
         System.out.println("Float: " + floatValue);
 
+        // long input and print
         System.out.print("Enter a long value: ");
         long longValue = scanner.nextLong();
         System.out.println("You entered: " + longValue);
 
+        // short input and print
         System.out.print("Enter a short value: ");
         short shortValue = scanner.nextShort();
         System.out.println("You entered: " + shortValue);
 
 
-
-        
 
         // operations and input values
         int resultInt = userInputInt + 5;
